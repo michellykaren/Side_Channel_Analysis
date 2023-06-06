4fac30676693f74ca6106839a0fa670457f0cd47
michellykaren <michellykaren15@gmail.com>
2023-06-06 14:37:36 +0200

    assembly code with C variables

--- hardware/victims/firmware/_trigger_test/trigger_test.c
+++ hardware/victims/firmware/_trigger_test/trigger_test.c
@@ -5,26 +5,27 @@
 
 int main(void)
 {
-   __asm (
-    "NOP \n\t" 
-    "NOP \n\t" 
-    "NOP \n\t" 
-
-    "mov r8, r8 \n\t"
-    "mov r0, r0 \n\t"
-    
-    "NOP \n\t" 
-    "NOP \n\t" 
-    "NOP \n\t" 
+    int s __attribute__((aligned (16)));
+    s = 0xBEBACA00;
+    __asm volatile (
+    "mov r1, #0 \n" 
+    "mov r1, #0x00FE \n" 
    );
 
-    while(1){
-     putch('h');
-     putch('e');
-     putch('l');
-     putch('l');
-     putch('o');
-     putch('\n');
-    }
-       
+   __asm volatile (
+    "ADD %[result], %[input_i], r1 \n"
+    : [result] "=r" (s)
+    : [input_i] "r" (s)
+    );  
+    return 0;
 }
+
+/*
+- Idx: Indica o índice da seção na tabela de seções. Os índices são números sequenciais atribuídos a cada seção.
+- Name: É o nome da seção, que é uma identificação única para a seção.
+- Size: É o tamanho da seção em bytes, indicando a quantidade de espaço ocupado pela seção na memória.
+- VMA (Virtual Memory Address): É o endereço virtual inicial da seção. O endereço virtual é o endereço lógico usado pelo programa durante a execução, que pode ser mapeado para um endereço físico específico pela unidade de gerenciamento de memória do sistema operacional.
+- LMA (Load Memory Address): É o endereço de carga inicial da seção. O endereço de carga é o endereço físico onde a seção é carregada na memória.
+- File off: É o deslocamento (offset) do início da seção no arquivo binário. Ele indica a posição da seção no arquivo.
+- Algn (Alignment): Indica o alinhamento da seção, ou seja, a restrição no endereço inicial da seção. É o valor de alinhamento em potência de 2 que especifica como a seção deve ser alinhada na memória.
+*/
